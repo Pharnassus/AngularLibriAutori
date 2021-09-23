01b66e581743023ec4a49880ad1ac7669f433bcf
Marian <unreality@live.it>
2021-09-23 17:25:03 +0200

    ciclato il json e creato una basilare istruzione di verifica sugli input

--- LibriAutoriV1/src/app/login/login.component.ts
+++ LibriAutoriV1/src/app/login/login.component.ts
@@ -14,32 +14,51 @@ export class LoginComponent implements OnInit {
 
   }
 
-  username : string;
+
+  // VARIABILI USATE NELLA FUNZIONE SUBMIT PER RECUPERARE I DATI DALL'INPUT
+  username : number;
   email : string;
   pwd : string;
 
+  // VARIABILI USATE NELLA FUNZIONE SUBMIT PER GENERARE IL BANNER LOGIN ERRATO
+  error = 0;
+  errorState = false;
+
   submit() {
 
-    const user= this.username;
-    const email= this.email;
-    const pwd= this.pwd;
-    console.log("user is " + user,"\n",
-                "email is " + email,"\n",
-                "password is " + pwd,"\n",
-                "JSON utenti:" + users[0].email,
-                );
+    const usernameInput= this.username;
+    const emailInput= this.email;
+    const pwdInput= this.pwd;
+    // console.log("user is " + user,"\n",
+    //             "email is " + email,"\n",
+    //             "password is " + pwd,"\n",
+    //             "JSON utenti:" + users[0].email,
+    //             );
+
+    // CICLA IL JSON
+    for (let i = 0; i < users.length; i++) {
+
+      // CICLA TUTTI GLI UTENTI; STESSA COSA POTRAI FARLA CON IL RESTO DELLE CHIAVI DEL JSON
+      // console.log(users[i].user);
+
+      // SE TROVA CORRISPONDENZE CON TUTTI E 3 I CAMPI DI INPUT STAMPA OK E SI FERMA ALTRIMENTI AUMENTA IL COUNTER error  DI 1
+      if (users[i].user == usernameInput && users[i].email == emailInput && users[i].pwd == pwdInput) {
+        alert("OK");
+        break
+      } else {
+        this.error++;
+      }
+
+      // SE CICLA TUTTO IL JSON E NON TROVA CORRISPONDENZE CON GLI UTENTI ALLORA STAMPA UN MESSAGGIO DI ERRORE
+      if (this.error == users.length) {
+        alert("KO");
+        this.errorState = true;
+        console.log(this.errorState);
+      }
+
+    }
 
   }
 
 }
 
-
-// loginSubmit(value){
-//   for(let i=0 ; i< this.login.length; i++)
-//   {
-//       if (this.login[i].username === value.username && this.login[i].password === value.password)
-//       {
-//           console.log("User Found" , this.login[i]);
-//       }
-//   }
-// }
