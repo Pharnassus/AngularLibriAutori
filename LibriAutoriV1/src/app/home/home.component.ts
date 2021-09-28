3be522aa04e4db818fa884b95fc571f9135d9289
Marian <unreality@live.it>
2021-09-28 16:39:21 +0200

    stampa di una card funzionante e senza errori

--- LibriAutoriV1/src/app/home/home.component.ts
+++ LibriAutoriV1/src/app/home/home.component.ts
@@ -24,29 +24,50 @@ export class HomeComponent implements OnInit {
   }[] = books_authors;
 
   // VARIABILI USATE NELLA FUNZIONE SUBMIT PER RECUPERARE I DATI DALL'INPUT
-  searchBook: string;
+  searchBookInput: string;
 
   // VARIABILI USATE NELLA FUNZIONE SUBMIT PER GENERARE I BANNER ERROR o BENVENUTO
-  error = 0;
+  // test : boolean;
+  error = 1;
+  counterJson : boolean;
   cardsShow = false;
   spinner = false;
-  arrayBooks = [];
+  arrayBooks: string[] = [];
 
-  searchBookFunction(){
+  searchBookFunction() {
 
-    const searchBook = this.searchBook;
+    let searchBookInput = this.searchBookInput;
+
+    //se non dovesse entrare il primo if questo counter true ti permetterà di entrare nell'if successivo
+    this.counterJson = true;
+    (<HTMLInputElement>document.getElementById('m_labelError')).innerHTML = 'Search result for';
 
 
+    //[ciclo] cicla il json
     for (let i = 0; i < books_authors.length; i++) {
 
-      if (searchBook == books_authors[i].author.name) {
+
+      if (
+        //prende l'input, lo trasforma in minuscolo, toglie gli spazi e confronta con un match nel json a cui vengono applicate le stesse regole
+        (searchBookInput).toLowerCase().replace(/\s+/g, '') == ((books_authors[i].name).toLowerCase().replace(/\s+/g, '')) ||
+        (searchBookInput).toLowerCase().replace(/\s+/g, '') == ((books_authors[i].author.name).toLowerCase().replace(/\s+/g, '')) ||
+        //la funzione includes è booleana: se la propr name nel json (in minuscolo e senza spazi) include l'input con le stesse regole allora è true
+        ((books_authors[i].name).toLowerCase().replace(/\s+/g, '').includes((searchBookInput).toLowerCase().replace(/\s+/g, ''))) ||
+        ((books_authors[i].author.name).toLowerCase().replace(/\s+/g, '').includes((searchBookInput).toLowerCase().replace(/\s+/g, '')))
+        ) {
+        //cancella l'array ad ogni click cosi da non duplicarlo
         this.arrayBooks.pop();
         this.spinner = true;
+        //finge di ricaricare le card
         this.cardsShow = false;
+        //switcha in false se la ricerca va a buon fine e non genera così errori nell'if seguente
+        this.counterJson = false;
 
+        //pusha la ricerca in un array provvisorio istanziato prima
         this.arrayBooks.push(books_authors[i]);
         console.log(this.arrayBooks);
 
+        //solo per grafica: ritarda di mezzo secondo la visualizzazione
         setTimeout(() => {
           this.spinner = false;
           this.cardsShow = true;
@@ -54,7 +75,32 @@ export class HomeComponent implements OnInit {
 
       }
 
+      //[ciclo] se non entra nel primo if azzera l'input e cambia il placeholder(label). Inoltre fa sparire le ricerche in pagina
+      if (this.counterJson) {
+        //per accedere all'id con il typescript bisogna aggiungere (<HTMLInputElement>)
+        (<HTMLInputElement>document.getElementById('m_searchBook')).value = '';
+        (<HTMLInputElement>document.getElementById('m_labelError')).innerHTML = 'Book or Author not found...';
+        this.cardsShow = false;
+      }
+
     }
+
+    //VALIDAZIONI FORM BOOTSTRAP
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
 
   }
 
