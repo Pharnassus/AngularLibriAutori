c6432f0cf27759854f9afdbcf3b2307d60e0a035
Marian <unreality@live.it>
2021-10-21 19:41:38 +0200

    tabella utenti inserita

--- LibriAutoriV1/src/app/admin-privileges/admin-privileges.component.ts
+++ LibriAutoriV1/src/app/admin-privileges/admin-privileges.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//IMPORTO IL JSON PER POTERLO USARE IN PAGINA
+import users from '../_services/users.json';
+
 @Component({
   selector: 'app-admin-privileges',
   templateUrl: './admin-privileges.component.html',
@@ -10,6 +13,15 @@ export class AdminPrivilegesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    for (let i = 0; i < users.length; i++) {
+
+      this.arrayUsers.push(users[i]);
+
+    }
+
   }
+
+  arrayUsers: string[] = [];
 
 }
